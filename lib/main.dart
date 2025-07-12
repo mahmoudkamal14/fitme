b4df0f2a4d702f72662b0d8315290ccdae1b1140
Mahmoud Kamal <https://github.com/mahmoudkamalx14>
2025-07-13 00:28:56 +0300

    modify main file

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'package:fitme/app/fitme_app.dart';
 import 'package:fitme/core/cache/cache_helper.dart';
 import 'package:fitme/core/routes/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  CacheHelper().init();
+
+  await CacheHelper().init();
+
+  await ScreenUtil.ensureScreenSize();
   runApp(FitmeApp(appRouter: AppRouter()));
 }
